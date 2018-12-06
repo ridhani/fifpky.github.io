5805a1ee6b6cab34d516b97ec475ee928d6b8621
ridhani <ridhani@gmail.com>
2018-12-06 09:06:45 +0700

    ok

---
+++ peta/kecamatan.js
@@ -0,0 +1,85 @@
+var datakecamatan=[{
+  "type": "FeatureCollection",
+  "features": [
+    {
+      "type": "Feature",
+      "properties": {
+        "kecamatan": "katingan",
+        "populasi": 100
+      },
+      "geometry": {
+        "type": "Polygon",
+        "coordinates": [
+          [ [
+           1624,2316
+            ],
+			 [
+            1535,2394
+            ],
+            [
+            1431,2454
+            ],
+            [
+             1365,2459
+            ],
+            [
+           1338,2493
+            ],
+            [
+           1300,2503
+            ],
+            [
+           1411,2684
+            ],
+            [
+           1471,2684
+            ],
+            [
+           1533,2611
+            ],
+            [
+          1647,2605
+            ],
+            [
+         1686,2438
+            ]
+          ]
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "kecamatan": "samba",
+        "populasi": 1000
+      },
+      "geometry": {
+        "type": "Polygon",
+        "coordinates": [
+          [
+            [
+              102,
+              58
+            ],
+            [
+              116,
+              56
+            ],
+            [
+              158,
+              67
+            ],
+            [
+              97,
+              66
+            ],
+            [
+              50,
+              58
+            ]
+          ]
+        ]
+      }
+    }
+  ]
+}];
